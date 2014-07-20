bc23a378ec83c2687d89169f38f59da7a0197e70
shivamk11 <shivamk11@gmail.com>
2014-07-20 15:44:10 +0530

    Add packing script for Moto G(falcon) Signed-off-by: shivamk11 <shivamk11@gmail.com>

---
+++ pack-falcon.sh
@@ -0,0 +1,41 @@
+# Script Pack zImage + Randisk
+# By Rachit Rawat
+# @XDA
+tput setaf 6
+setterm -bold 
+echo "**** IMAGE PACKER SCRIPT ****"
+echo "**** FOR falcon ****"
+echo "**** By Rachit Rawat ****"
+tput sgr0 
+setterm -bold
+echo "Checking zImage in kernel/zImage"
+if test -e kernel/zImage-dtb
+   then echo "zImage found"
+else echo "Kernel not found!"
+   tput sgr0
+   exit 
+fi
+echo "Checking Ramdisk"
+if test -d ramdisk 
+then echo "Ramdisk found" 
+else echo "Ramdisk not found!"
+tput sgr0
+exit 
+fi
+
+echo "zImage + ramdisk found, preparing ramdisk"
+sleep 2 
+./tools/mkbootfs ramdisk | gzip > ramdisk.gz
+sleep 2
+echo "Packing final Kernel (boot.img) "
+mkdir -p out
+./tools/mkbootimg --kernel kernel/zImage-dtb --ramdisk ramdisk.gz -o out/boot.img --base 0x00000000 --ramdisk_offset 0x01000000 --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags androidboot.write_protect=0 zcache" --pagesize 2048
+sleep 2
+setterm -bold 
+rm ramdisk.gz
+echo "boot.img ready! Check out folder"
+tput sgr0
+setterm -bold
+echo "All Done, press enter to exit"
+tput sgr0
+read ANS
